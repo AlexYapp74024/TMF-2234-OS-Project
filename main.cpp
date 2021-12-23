2d35463875b0360d9362980fe25b56080b770e97
AlexYapp74024 <74024@siswa.unimas.my>
2021-12-23 22:44:13 +0800

    Testing Live Share
    Co-authored-by: shenfluke <shenfluke@users.noreply.github.com>
    Co-authored-by: Voon-Zhi-Hao <Voon-Zhi-Hao@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -3,7 +3,13 @@ using namespace std;
 
 int main()
 {
-    cout << "hello";
+    cout << "hello" << endl;
+    cout << "test" << endl;
+
+    cout << "Hello World" << endl;
 
     return 0;
 }
+
+// Extract Data
+// Process management
