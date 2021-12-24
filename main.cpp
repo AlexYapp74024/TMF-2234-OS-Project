a36be190cd536c715a0bd92dd623c8634f922b2e
AlexYapp74024 <74024@siswa.unimas.my>
2021-12-24 20:07:53 +0800

    Partially completed csv parsing
    Co-authored-by: Voon-Zhi-Hao <Voon-Zhi-Hao@users.noreply.github.com>
    Co-authored-by: shenfluke <shenfluke@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -1,15 +1,46 @@
 #include <iostream>
+#include <vector>
+
+#include "csv parsing.cpp"
+#include "round robin.cpp"
+
 using namespace std;
+
+pair<vector<vector<string>>, vector<vector<int>>> getTestData(vector<int> &out_arrivalTime)
+{
+    // I/O (31)
+    vector<vector<string>> processType;
+    vector<vector<int>> cycles;
+
+    processType = {{"I/O", "CPU", "CPU"},
+                   {"CPU", "CPU", "I/O"},
+                   {"CPU", "CPU", "CPU"}};
+
+    cycles = {{30, 21, 13},
+              {17, 5, 21},
+              {9, 2, 11}};
+
+    out_arrivalTime = {1, 2, 36};
+
+    return make_pair(processType, cycles);
+}
 
 int main()
 {
-    cout << "hello" << endl;
-    cout << "test" << endl;
+    const string FILENAME = "Job List 11.csv";
 
-    cout << "Hello World" << endl;
+    pair<vector<vector<string>>, vector<vector<int>>> processes;
+    vector<int> arrivalTime;
+
+    // processes = getTestData(arrivalTime);
+
+    parseCSV(FILENAME, arrivalTime);
+
+    // for (auto i : arrivalTime)
+    //     cout << i << " ";
+
+    cout << "\nOperation Successful!!\n"
+         << endl;
 
     return 0;
 }
-
-// Extract Data
-// Process management
